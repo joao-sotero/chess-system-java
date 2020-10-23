06be2fafb55af711006e42490772814f2d094031
joao-sotero <vitor99jjunior@gmail.com>
2020-10-22 22:48:27 -0300

    Print out board end piece of board

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -18,11 +18,11 @@ public class Position {
 		this.row = row;
 	}
 
-	public int getColor() {
+	public int getColumn() {
 		return column;
 	}
 
-	public void setColor(int column) {
+	public void setColumn(int column) {
 		this.column = column;
 	}
 	
